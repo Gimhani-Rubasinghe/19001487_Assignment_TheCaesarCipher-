6a4a6a4d1ce8ab3705565b220810eab028c6c6ab
Gimhani-Rubasinghe <62197172+Gimhani-Rubasinghe@users.noreply.github.com>
2021-08-09 17:53:30 +0530

    Add files via upload

--- 19001487/CaesarCipher.scala
+++ 19001487/CaesarCipher.scala
@@ -1,10 +1,10 @@
 object CaesarCipher extends App{
   val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   val Encript = (c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)+key)%a.size)
-  val Decript = (c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)-key)%a.size)
+  val Decript = (c:Char,key:Int,a:String)=>a(if (a.indexOf(c.toUpper)-key>0) ((a.indexOf(c.toUpper)-key)%a.size) else (a.indexOf(c.toUpper)+a.size-key)%a.size)
   val cipher =(algo:(Char,Int,String)=>Char,s:String,key:Int,a:String)=>s.map(algo(_,key,a))
 
-  val s = "ASSIGNMENT" //input string
+  val s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" //input string
   val cipertext=cipher(Encript,s,5,alphabet)
   val plaintext=cipher(Decript,cipertext,5,alphabet)
   println("Text : " + s)
